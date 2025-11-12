4763c7ae30c3da0280dc254def3e163a4645952b
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-12 13:27:45 +0330

    update cdec query

--- nexus/cdec.py
+++ nexus/cdec.py
@@ -1,18 +1,16 @@
 import sys
 import os
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config import *
 import requests
 import pandas as pd
 from io import StringIO
-import os
 
 os.makedirs("../dataset", exist_ok=True)
-response = requests.get(base_url, params=params)
+response = requests.get(cdec_base_url, params=cdec_params)
 if response.status_code == 200:
     df = pd.read_csv(StringIO(response.text))
-    df.to_csv(save_path, index=False)
-    print("Data saved to " + save_path)
+    df.to_csv(cdec_save_path, index=False)
+    print("CDEC Data Saved.")
 else:
     print(f"Error: {response.status_code} - {response.text}")
