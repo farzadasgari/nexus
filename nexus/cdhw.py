5a7c245037b115e97fbf436d1c61bb9081f39cc9
Farzad Asgari <khufarzadasgari@gmail.com>
2025-11-22 16:20:02 +0330

    handle leap years in cdhw

--- nexus/cdhw.py
+++ nexus/cdhw.py
@@ -5,4 +5,15 @@ data = pd.read_csv("../dataset/climate_features.csv", low_memory=False)
 data['Date'] = pd.to_datetime(data['Date'])
 data.set_index('Date', inplace=True)
 
+for col in ['PRCP', 'TMAX', 'TMIN']:
+    data[col] = pd.to_numeric(data[col], errors='coerce')
 
+leap_years = data.index[data.index.is_leap_year].year.unique()
+for year in leap_years:
+    feb28 = data.loc[f"{year}-02-28"]
+    feb29 = data.loc[f"{year}-02-29"]
+    averaged = (feb28[['PRCP', 'TMAX', 'TMIN']] + feb29[['PRCP', 'TMAX', 'TMIN']]) / 2
+    data.loc[f"{year}-02-28", ['PRCP', 'TMAX', 'TMIN']] = averaged
+data = data[~((data.index.month == 2) & (data.index.day == 29))]
+
+data['TMEAN'] = data[['TMAX', 'TMIN']].mean(axis=1)
